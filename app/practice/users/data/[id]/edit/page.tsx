51a5e817acb92cfe9bfd4da4055f17ecded59d95
asrim10 <asrimmsuwall@gmail.com>
2025-12-12 10:26:22 +0545

    nextjs_url_Practice

---
+++ app/practice/users/data/[id]/edit/page.tsx
@@ -0,0 +1,10 @@
+export default async function Page({
+    params
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+    return (
+        <div> Edit User Data Page: {id}</div>
+    );
+}
