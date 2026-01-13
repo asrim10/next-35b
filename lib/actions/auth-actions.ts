d989263c5824db96afec49a1dd348f7d1594d512
asrim10 <asrimmsuwall@gmail.com>
2026-01-13 10:00:14 +0545

    auth_api_actions

---
+++ lib/actions/auth-actions.ts
@@ -0,0 +1,21 @@
+// server side processing
+
+"use server";
+import { register } from "../api/auth";
+export const handleRegister = async (formData: any) => {
+  try {
+    //how data sent from component to backend api
+    const res = await register(formData);
+    //component return logic
+    if (res.success) {
+      return {
+        success: true,
+        data: res.data,
+        message: "Registration Successful",
+      };
+    }
+    return { success: false, message: res.message || "Registration Failed" };
+  } catch (err: Error | any) {
+    return { success: false, message: err.message || "Registration Failed" };
+  }
+};
---
+++ lib/api/auth.ts
@@ -0,0 +1,17 @@
+//Actual backend API calls
+import axios from "./axios"; // IMPORTANT: axios instance with base URL
+import { API } from "./endpoints";
+
+export const register = async (registerData: any) => {
+  try {
+    const response = await axios.post(API.AUTH.REGISTER, registerData);
+    return response.data; // response ko body (what backend returns)
+  } catch (err: Error | any) {
+    //if 4xx/5xx error, axios throws error
+    throw new Error(
+      err.response?.data?.message || //backend error message
+        err.message || // general axios error message
+        "Registration failed" // fallback message
+    );
+  }
+};
