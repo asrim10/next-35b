73e441e6b0a7feba5a1899c572b41e016a9d1230
asrim10 <asrimmsuwall@gmail.com>
2026-01-14 11:41:32 +0545

    login_actions

--- lib/action/auth-actions.ts
+++ lib/action/auth-actions.ts
@@ -1,7 +1,7 @@
 // server side processing
 
 "use client";
-import { register } from "../api/auth";
+import { login, register } from "../api/auth";
 export const handleRegister = async (formData: any) => {
   try {
     //how data sent from component to backend api
@@ -17,5 +17,23 @@ export const handleRegister = async (formData: any) => {
     return { success: false, message: res.message || "Registration Failed" };
   } catch (err: Error | any) {
     return { success: false, message: err.message || "Registration Failed" };
+  }
+};
+
+export const handlelogin = async (formData: any) => {
+  try {
+    //how data sent from component to backend api
+    const res = await login(formData);
+    //component return logic
+    if (res.success) {
+      return {
+        success: true,
+        data: res.data,
+        message: "Login Successful",
+      };
+    }
+    return { success: false, message: res.message || "Login Failed" };
+  } catch (err: Error | any) {
+    return { success: false, message: err.message || "Login Failed" };
   }
 };
