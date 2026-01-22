33117f8349bbd37555a0676d25b9036580a95461
asrim10 <asrimmsuwall@gmail.com>
2026-01-22 07:40:46 +0545

    error_and_notFound_boundary

--- app/example/server-boundary/page.tsx
+++ app/example/server-boundary/page.tsx
@@ -1,6 +1,16 @@
 import { exampleAction } from "@/lib/action/example-actions";
+import { notFound } from "next/navigation";
 
 export default async function Page() {
   const result = await exampleAction();
+
+  if (result.success) {
+    throw new Error("Error");
+  }
+
+  if (result.data == null) {
+    notFound();
+  }
+
   return <div>Server Boundary Page</div>;
 }
